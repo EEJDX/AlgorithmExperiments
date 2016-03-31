060d98cd4c7553698a2e3bd3b17a0ba3e82e6ec3
EEJDX <gmalejd@gmail.com>
2016-03-31 18:33:40 -0500

    Russian Peasant Algorithm
    Adds a Russian Peasant algorithm that is significantly faster than
    looping multiplication

--- naive.py
+++ naive.py
@@ -6,3 +6,15 @@ def naive(a, b):
         z = z + y
         x = x - 1
     return z
+
+
+def russian(a, b):
+    x = a; y = b
+    z = 0
+    while x > 0:
+        if x % 2 == 1: z = z + y
+        y = y << 1
+        x = x >> 1
+    return z
+
+
