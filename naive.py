cf8209725c1d7734f04cc0f751a84fbf5c2e4888
EEJDX <gmalejd@gmail.com>
2016-04-18 21:31:49 -0500

    Eulerian Tour
    Initial commit of Eulerian Tour

--- naive.py
+++ naive.py
@@ -18,3 +18,7 @@ def russian(a, b):
     return z
 
 
+def rec_russian(a, b):
+    if a == 0: return 0
+    if a % 2 == 0: return 2 * rec_russian(a / 2, b)
+    return b + (2 * rec_russian((a - 1) / 2, b)
