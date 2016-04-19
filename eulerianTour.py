caade5e4d21d24f1141ab6bc89fcad4c3a67c92e
EEJDX <gmalejd@gmail.com>
2016-04-18 22:07:54 -0500

    Tour and Graph
    Adds tour-from-graph and graph-from-tour

--- eulerianTour.py
+++ eulerianTour.py
@@ -144,3 +144,40 @@ def test():
     nodes = [20, 21, 22, 23, 24, 25]
     tour = create_tour(nodes)
     return is_eulerian_tour(nodes, tour)
+
+
+
+
+
+
+
+
+# Find Eulerian Tour
+#
+# Write a function that takes in a graph
+# represented as a list of tuples
+# and return a list of nodes that
+# you would follow on an Eulerian Tour
+#
+# For example, if the input graph was
+# [(1, 2), (2, 3), (3, 1)]
+# A possible Eulerian tour would be [1, 2, 3, 1]
+
+def find_eulerian_tour(graph):
+    tour=[]
+    find_tour(graph[0][0],graph,tour)
+    return tour
+
+def find_tour(u,E,tour): 
+  for (a,b) in E:
+    if a==u:
+        E.remove((a,b))
+        find_tour(b,E,tour)
+    elif b==u:
+        E.remove((a,b))
+        find_tour(a,E,tour)
+  tour.insert(0,u)
+
+
+
+
--- naive.py
+++ naive.py
@@ -7,18 +7,29 @@ def naive(a, b):
         x = x - 1
     return z
 
+def rec_naive(a, b):
+    print("count me!")
+    if a == 0:
+        return 0
+    return b + rec_naive(a-1, b)
+
 
 def russian(a, b):
     x = a; y = b
     z = 0
     while x > 0:
+        if x == 7 and z == 84: print(y)
         if x % 2 == 1: z = z + y
         y = y << 1
         x = x >> 1
     return z
 
+def countClique(n):
+    return 2 + n + (n * (n - 1)) / 2
 
-def rec_russian(a, b):
-    if a == 0: return 0
-    if a % 2 == 0: return 2 * rec_russian(a / 2, b)
-    return b + (2 * rec_russian((a - 1) / 2, b)
+
+def clique(n):
+    print ("in a clique...")
+    for j in range(n):
+        for i in range(j):
+            print (i, " is friends with ", j)
